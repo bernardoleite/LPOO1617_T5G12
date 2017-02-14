d72f745e317eff27133abc95133def35d845e7af
etielneb <ben_leite_@hotmail.com>
2017-02-14 22:49:29 +0000

    Primeiro Commit - Imprime tabuleiro jogo

---
+++ LPOOtrabalhos/src/jogo.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class jogo {
+    
+    
+
+
+    public static void main(String[] args) {
+    	
+    	int i =0, j = 0;
+    	
+    	char vec[][] = {{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}, 
+    			{'X', 'H', ' ', ' ', 'I', ' ', 'X', ' ', 'G', 'X'},
+    			{'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+    			{'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+    			{'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+    			{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+    			{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+    			{'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X'},
+    			{'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'},
+    			{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}
+    		};
+    	    	
+    	;
+    	;
+    	;
+    	
+    	for(i = 0; i < 10; i++){
+    		for (j = 0 ; j < 10; j++){
+    			System.out.print (vec[i][j]);
+    			System.out.print(" ");
+    			;
+    			;
+    			
+    		}
+    		System.out.print("\n");
+    	}
+    	
+    	
+    
+
+}}
